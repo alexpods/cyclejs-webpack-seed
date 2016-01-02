b0a4ffcdd775f8c59f22178301aa7db9b0fb7988
alexpods <aleksey.podskrebyshev@gmail.com>
2016-01-02 21:53:04 +0300

    BMI exmaple

--- src/index.jsx
+++ src/index.jsx
@@ -2,12 +2,32 @@ import { run } from '@cycle/core';
 import { makeDOMDriver, hJSX } from '@cycle/dom';
 import { Observable } from 'rx';
 
-function main() {
+function main({ DOM }) {
+
+  const changeWeight$ = DOM.select('#weight').events('change').map(event => event.target.value);
+  const changeHeight$ = DOM.select('#height').events('change').map(event => event.target.value);
+
+  const bmi$ = Observable.combineLatest(
+    changeWeight$.startWith(40),
+    changeHeight$.startWith(140),
+    (weight, height) => {
+      const heightMeters = height * 0.01;
+      return Math.round(weight / (heightMeters * heightMeters));
+    }
+  );
+
+  const vdom$ = bmi$
+    .map((bmi) => (
+      <div>
+        <div>Weight: <input id="weight" type="range" min="40" max="140" /></div>
+        <div>Height: <input id="height" type="range" min="140" max="210" /></div>
+        <h1>BMI: {bmi}</h1>
+      </div>
+    ));
+
   return {
-    DOM: Observable.from([
-      <h1>Hello, world!</h1>
-    ])
-  }
+    DOM: vdom$
+  };
 }
 
 run(main, {
