10d4c6fd652f770d6d3cf8fd2b73010b5faecd7f
alexpods <aleksey.podskrebyshev@gmail.com>
2016-01-02 21:39:29 +0300

    Increment example

--- src/index.jsx
+++ src/index.jsx
@@ -2,11 +2,26 @@ import { run } from '@cycle/core';
 import { makeDOMDriver, hJSX } from '@cycle/dom';
 import { Observable } from 'rx';
 
-function main() {
+function main({ DOM }) {
+
+  const action$ = Observable.merge(
+    DOM.select('#increment').events('click').map(_ => 1),
+    DOM.select('#decrement').events('click').map(_ => -1)
+  );
+
+  const count$ = action$.startWith(0).scan((x, y) => x + y);
+
+  const vdom$ = count$
+      .map((count) => (
+        <div>
+          <button id="increment">Increment</button>
+          <button id="decrement">Decrement</button>
+          <p>{count}</p>
+        </div>
+      ));
+
   return {
-    DOM: Observable.from([
-      <h1>Hello, world!</h1>
-    ])
+    DOM: vdom$
   }
 }
 
