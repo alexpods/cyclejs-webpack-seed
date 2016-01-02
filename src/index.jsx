efa90c54d6dd6e89f00b4e0ba686cf8ec3746d54
alexpods <aleksey.podskrebyshev@gmail.com>
2016-01-02 22:38:35 +0300

    MVI version of BMI example

--- src/index.jsx
+++ src/index.jsx
@@ -2,31 +2,67 @@ import { run } from '@cycle/core';
 import { makeDOMDriver, hJSX } from '@cycle/dom';
 import { Observable } from 'rx';
 
-function main({ DOM }) {
+function intent(DOM) {
+  return {
+    changeWeight$: DOM.select('#weight').events('change').map(event => event.target.value),
+    changeHeight$: DOM.select('#height').events('change').map(event => event.target.value)
+  };
+}
 
-  const changeWeight$ = DOM.select('#weight').events('change').map(event => event.target.value);
-  const changeHeight$ = DOM.select('#height').events('change').map(event => event.target.value);
-
-  const bmi$ = Observable.combineLatest(
+function model({ changeWeight$, changeHeight$ }) {
+  return Observable.combineLatest(
     changeWeight$.startWith(40),
     changeHeight$.startWith(140),
-    (weight, height) => {
-      const heightMeters = height * 0.01;
-      return Math.round(weight / (heightMeters * heightMeters));
-    }
+    (weight, height) => ({
+      weight,
+      height,
+      bmi: calculateBMI(weight, height)
+    })
+  )
+}
+
+function view(state$) {
+  return state$.map(({ weight, height, bmi }) => (
+    <div>
+      {renderWeightInput(weight)}
+      {renderHeightInput(height)}
+      {renderBMI(bmi)}
+    </div>
+  ));
+}
+
+function renderWeightInput(weight) {
+  return (
+    <div>
+      Weight: <input id="weight" type="range" min="40" max="140" value={weight} />
+    </div>
   );
+}
 
-  const vdom$ = bmi$
-    .map((bmi) => (
-      <div>
-        <div>Weight: <input id="weight" type="range" min="40" max="140" /></div>
-        <div>Height: <input id="height" type="range" min="140" max="210" /></div>
-        <h1>BMI: {bmi}</h1>
-      </div>
-    ));
+function renderHeightInput(height) {
+  return (
+    <div>
+      Height: <input id="height" type="range" min="140" max="210" value={height} />
+    </div>
+  );
+}
 
+function renderBMI(bmi) {
+  return (
+    <h1>
+      BMI: {bmi}
+    </h1>
+  );
+}
+
+function calculateBMI(weight, height) {
+  const heightMeters = height * 0.01;
+  return Math.round(weight / (heightMeters * heightMeters));
+}
+
+function main({ DOM }) {
   return {
-    DOM: vdom$
+    DOM: view(model(intent(DOM)))
   };
 }
 
